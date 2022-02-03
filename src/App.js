da7b7c0d8f3bfe1d03a18c5997044655ec122195
Halil Özat <halilozat@hotmail.com>
2022-02-03 21:27:03 +0300

    added v

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { motion } from "framer-motion"
 
 function App() {
+
+  const variants = {
+    visiable: {
+      opacity: 1,
+      transition: {
+        duration: 2,
+      }
+    },
+    hidden: {
+      opacity: 0,
+    }
+  }
+
   return (
-    <div className="App">
-      
+    <div>
+      <motion.ul 
+      className="box" 
+      initial="hidden"
+      animate="visiable"
+      variants={variants}
+      >
+        <li style={{margin: 20, width: 20, height: 20, backgroundColor: 'black'}} />
+        <li style={{margin: 20, width: 20, height: 20, backgroundColor: 'black'}} />
+      </motion.ul>
     </div>
   );
 }
