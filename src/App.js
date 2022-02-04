1cf43a705b5dbddd0c41e63377cdd970912410bd
Halil Özat <halilozat@hotmail.com>
2022-02-04 12:04:41 +0300

    after code

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import { motion, useAnimation } from "framer-motion"
 
 function App() {
 
-  const controls = useAnimation()
+  const control = useAnimation()
 
+  //variant
   return (
-    <div>
-      <motion.div 
-      className="box" 
-      animate={controls}
+    <div className="container" >
+      <motion.div className="box"
+        animate={control}
       />
 
-      <button style={{marginTop:'200px'}} onClick={() => controls.start({
-        scale: 0.5,
-        x: 200,
-        transition: {
-          type: 'spring',
-          bounce: 0.7,
-        }
-      })}>Start</button>
-      <button style={{marginTop:'200px'}} onClick={() => controls.stop()}>Stop</button>
+
+      <button
+        onClick={() => control.start({
+          scale: 0.5,
+          x: 200,
+          transition: {
+            type: 'spring',
+            bounce: 0.7,
+            duration: 2,
+            repeat: Infinity
+          }
+        })}
+        style={{ marginTop: '200px' }} >
+        Start
+      </button>
+
+      <button
+        onClick={() => control.stop()}
+        style={{ marginTop: '200px' }} >
+        Stop
+      </button>
     </div>
+
   );
 }
 
